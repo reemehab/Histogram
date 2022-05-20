55674e0ad3569ad27faf2308130e80c85466f20f
reemehab <60987184+reemehab@users.noreply.github.com>
2022-05-21 01:17:13 +0200

    "C Code with mpi "Scatter and Gather Functions""

--- main.c
+++ main.c
@@ -1,61 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 struct pointer
 {
-    int start ;
+    int start;
     int end;
 };
 int isBetween(int number, int start, int end)
 {
-    if(number >= start && number <end)
+    if (number >= start && number < end)
         return 1;
     return 0;
 }
-int main()
-{
-    FILE *ptr;
-    ptr = fopen("D:\\codeblocks\\hpc3\\dataset.txt", "r");
-    int numBars;
-    printf("Enter number of Bars : \n");
-    scanf("%d", &numBars);
+int main(int argc, char *argv[])
 
+{
+    int i, j;
+    int my_rank; // rank of process    /
+    int p;       // number of process    */
+    int index, x, count = 0;
+    int number, sizee;
+    int portionSize, *portionArr;
+    int *sums;
+    FILE *ptr;
+    int *array;
+
+    ptr = fopen("/shared/dataset.txt", "r");
+
+    int numBars;
+    //    printf("Enter number of Bars : \n");
+    // scanf("%d", &numBars);
+    numBars = 2;
+    int *counter = malloc(numBars * sizeof(int)), localCounter[numBars];
     struct pointer points[numBars];
     int start, end;
-    printf("Please Enter %d points in each line :", numBars);
-    for(int i=0 ; i<numBars ; i++)
+    //  printf("Please Enter %d points in each line :", numBars);
+    /*for( i=0 ; i<numBars ; i++)
     {
         scanf("%d %d", &start, &end);
         points[i].start = start;
         points[i].end = end;
-    }
-    if (NULL == ptr)
+    }*/
+    points[0].start = 0;
+    points[0].end = 15;
+    points[1].start = 15;
+    points[1].end = 30;
+    // printf("please enter size of array \n");
+    // scanf("%d", &sizee);
+    sizee = 20;
+
+    for (i = 0; i < numBars; i++)
     {
-        printf("file can't be opened \n");
+        counter[i] = 0;
+        localCounter[i] = 0;
     }
-    int number, sizee;
-    printf("please enter size of array \n");
-    scanf("%d", &sizee);
-    int array[sizee], index, x, count =0;
-    index = fscanf(ptr, "%d", &x);
-    while (index != EOF) //taking the data points from the file and assigning it to the array
+
+    MPI_Status status; // return status for recieve
+
+    // Start up MPI
+    MPI_Init(&argc, &argv);
+
+    // Find out process rank /
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    // Find out number of process */
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    if (my_rank == 0)
     {
-        array[count++] = x;
-        index = fscanf(ptr, "%d", &x );
-    }
-    int counter[numBars];
-    for(int i=0 ; i< numBars ; i++)
-        counter[i]=0;
-    for(int i=0 ; i < sizee ; i++ )
-    {
-        for(int j =0 ; j < numBars ; j++ )
+
+        if (NULL == ptr)
         {
-            if(isBetween(array[i], points[j].start , points[j].end) == 1)
-                counter[j]++;
+            printf("file can't be opened \n");
+        }
+        array = malloc(sizee * sizeof(int));
+        index = fscanf(ptr, "%d", &x);
+        while (index != EOF) // taking the data points from the file and assigning it to the array
+        {
+            array[count++] = x;
+            index = fscanf(ptr, "%d", &x);
+        }
+
+        portionSize = sizee / (p);
+        // int remainder = sizee - (portionSize * p );
+    }
+    MPI_Bcast(&portionSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    portionArr = malloc(portionSize * sizeof(int));
+    /* for(; i<sizee; i++){
+      printf("my Rank = %d , Arr= %d \n" , my_rank ,portionArr);
+
+  }*/
+    MPI_Scatter(array, portionSize, MPI_INT, portionArr, portionSize, MPI_INT, 0, MPI_COMM_WORLD);
+    i = 0;
+    for (; i < portionSize; i++)
+    {
+        for (j = 0; j < numBars; j++)
+        {
+            if (isBetween(portionArr[i], points[j].start, points[j].end) == 1)
+            {
+                localCounter[j]++;
+                // printf("my Rank = %d , partionArr= %d \n" , my_rank ,portionArr[i]);
+            }
         }
     }
-    for(int i=0 ; i < numBars ; i++)
-        printf("%d \n" , counter[i]);
 
+    MPI_Barrier(MPI_COMM_WORLD);
+    sums = malloc(p * sizeof(int));
+    j = 0;
+    for (;j < numBars; j++)
+    {
+        MPI_Allgather(&localCounter[j], 1, MPI_INT, sums, 1, MPI_INT, MPI_COMM_WORLD);
+        for (i = 0; i < p; i++)
+        {
+            counter[j] += sums[i];
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == 0)
+    {   i=0;
+        for (; i < numBars; i++)
+        {
+            printf(" The range start with %d, end with %d , with count %d \n ", points[i].start, points[i].end, counter[i]);
+        }
+    }
+    MPI_Finalize();
     fclose(ptr);
 
     return 0;
